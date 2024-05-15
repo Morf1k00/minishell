07d2d40e124323f89c1c374fda7931da82bf5bc2
Denis <debizhan@student.42wolfsburg.de>
2024-05-15 16:12:22 +0200

    managed list creating

---
+++ tokens.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokens.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: debizhan <debizhan@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/15 15:47:07 by debizhan          #+#    #+#             */
+/*   Updated: 2024/05/15 16:11:00 by debizhan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static t_vars	new_list(char *arg)
+{
+	t_vars	*list;
+
+	list = malloc(sizeof(t_vars));
+	if (!list)
+		return (NULL);
+	list->lenght = ft_strlen(arg);
+	list->type = return_token_type();
+	list->token = arg;
+	list->next = NULL;
+	return (list);
+}
+
+static void	list_add(t_vars **lst, t_vars *new)
+{
+	t_vars	*last;
+
+	if (!lst || !new)
+		return ;
+	if (*lst == NULL)
+	{
+		*lst = new;
+		return ;
+	}
+	last = *lst;
+	while (last->next != NULL)
+		last = last->next;
+	last->next = new;
+}
+
+int	create_list(t_vars **list, char **arv)
+{
+	int	i;
+
+	i = 0;
+	if (!list || !arv)
+		return (0)
+	while (arv[i])
+		list_add(list, new_list(arv[i++]))
+	return (1);
+}
