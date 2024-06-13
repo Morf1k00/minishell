e834fb0584db7e37ec2bd91c98a1dea8f5437042
Denis <debizhan@student.42wolfsburg.de>
2024-06-13 18:15:18 +0200

    testing

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: debizhan <debizhan@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 10:29:35 by rkrechun          #+#    #+#             */
-/*   Updated: 2024/06/13 17:59:03 by debizhan         ###   ########.fr       */
+/*   Updated: 2024/06/13 18:12:04 by debizhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,39 @@ void	print_list(t_vars **lst)
 		//printf("info: %s\ttype: %d\tlength: %d\n", tmp->token, tmp->type, tmp->length);
 		tmp = tmp->next;
 	}
+}
+
+int	main(int argc, char **argv, char **env)
+{
+	char		*input;
+	char		**line;
+	t_env_path	*env_shell;
+	t_vars		*list;
+
+	list = NULL;
+	env_shell = malloc(sizeof(t_env_path));
+	init_arg(argc, argv, env, env_shell);
+	while (1)
+	{
+		input = readline("minishell: ");
+		add_history(input);
+		line = split_arg(input);
+		if (close_quote(line))
+			lexer(line, env_shell);
+		else
+		{
+			printf("quote not close\n");
+			exit_file(env_shell);
+		}
+		if (!create_list(&list, env_shell->pipes->arv, env_shell))
+		{
+			printf("something went wrong\n");
+			exit_file(env_shell);
+		}
+		execute_command(list, env_shell);
+		ft_listclear(&list);
+	}
+	free(input);
 }
 
 int	main(int argc, char **argv, char **env)
