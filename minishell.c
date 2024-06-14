2cb8ef246a048833df477ba9bf920dc9624518dc
Denis <debizhan@student.42wolfsburg.de>
2024-06-14 14:47:28 +0200

    fixing...

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: debizhan <debizhan@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 10:29:35 by rkrechun          #+#    #+#             */
-/*   Updated: 2024/06/13 18:12:04 by debizhan         ###   ########.fr       */
+/*   Updated: 2024/06/13 19:10:33 by debizhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,35 +65,35 @@ int	main(int argc, char **argv, char **env)
 	free(input);
 }
 
-int	main(int argc, char **argv, char **env)
-{
-	char		*input;
-	char		**line;
-	t_env_path	*env_shell;
-	t_vars		*list;
+// int	main(int argc, char **argv, char **env)
+// {
+// 	char		*input;
+// 	char		**line;
+// 	t_env_path	*env_shell;
+// 	t_vars		*list;
 
-	list = NULL;
-	env_shell = malloc(sizeof(t_env_path));
-	init_arg(argc, argv, env, env_shell);
-	while (1)
-	{
-		input = readline("minishell: ");
-		add_history(input);
-		line = split_arg(input);
-		if (close_quote(line))
-			lexer(line, env_shell);
-		else
-		{
-			printf("quote not close\n");
-			exit_file(env_shell);
-		}
-		if (!create_list(&list, env_shell->pipes->arv, env_shell))
-		{
-			printf("something went wrong\n");
-			exit_file(env_shell);
-		}
-		execute_command(list, env_shell);
-		ft_listclear(&list);
-	}
-	free(input);
-}
+// 	list = NULL;
+// 	env_shell = malloc(sizeof(t_env_path));
+// 	init_arg(argc, argv, env, env_shell);
+// 	while (1)
+// 	{
+// 		input = readline("minishell: ");
+// 		add_history(input);
+// 		line = split_arg(input);
+// 		if (close_quote(line))
+// 			lexer(line, env_shell);
+// 		else
+// 		{
+// 			printf("quote not close\n");
+// 			exit_file(env_shell);
+// 		}
+// 		if (!create_list(&list, env_shell->pipes->arv, env_shell))
+// 		{
+// 			printf("something went wrong\n");
+// 			exit_file(env_shell);
+// 		}
+// 		execute_command(list, env_shell);
+// 		ft_listclear(&list);
+// 	}
+// 	free(input);
+// }
