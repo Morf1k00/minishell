15e42a72b212d7c7f3a893a0009c843f1da89a74
Denis <debizhan@student.42wolfsburg.de>
2024-07-02 16:33:33 +0200

    merged

--- libft/ft_strcmp.c
+++ libft/ft_strcmp.c
@@ -5,9 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: debizhan <debizhan@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-<<<<<<< HEAD:libft/ft_strcmp.c
 /*   Created: 2024/07/02 14:00:24 by debizhan          #+#    #+#             */
-/*   Updated: 2024/07/02 14:00:37 by debizhan         ###   ########.fr       */
+/*   Updated: 2024/07/02 16:33:19 by debizhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +21,3 @@ int ft_strcmp(const char *s1, const char *s2)
     }
     return *(unsigned char *)s1 - *(unsigned char *)s2;
 }
-=======
-/*   Created: 2024/06/03 16:08:56 by rkrechun          #+#    #+#             */
-/*   Updated: 2024/07/01 13:10:35 by rkrechun         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "minishell.h"
->>>>>>> rostik:quots2.c
